4907f458e901358b9f63e90fd16e703abe241895
Mahi <mainul.i.mahi@gmail.com>
2015-03-30 02:19:03 +0600

    Initial Commit

---
+++ CMSByMVCTest/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CMSByMVCTest.Models;
+
+namespace CMSByMVCTest.Areas.Admin.Controllers
+{
+
+    [RouteArea("Admin")]
+    [RoutePrefix("post")]
+    public class PostController : Controller
+    {
+        //
+        // GET: /Admin/Post/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //admin/post/create/
+        [Route("create")]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var model = new Post();
+            return View(model);
+        }
+
+        //admin/post/create/
+        [Route("create")]
+        [HttpPost]
+        public ActionResult Create(Post model) {
+            if (!ModelState.IsValid) {
+                return View(model);
+            }
+
+            //update model in data store
+            return RedirectToAction("index");
+        }
+
+
+        //admin/post/edit/post-to-edit
+        [Route("edit/{id}")]
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            var model = new Post();
+
+            return View(model);
+        }
+
+
+        //admin/post/edit/post-to-edit
+        [Route("edit/{id}")]
+        [HttpPost]
+        public ActionResult Edit(Post model) {
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            //update model in data store
+            return RedirectToAction("index");
+
+        }
+	}
+}
